8cb284144c0343eb4f77f70607e945014b84df3d
This-Bmoreira <contatobrunosmoreira@gmail.com>
2023-11-15 17:36:41 -0300

    test: add validation check in UserEntity unit test

--- test/domain/user/unit/user.entity.spec.ts
+++ test/domain/user/unit/user.entity.spec.ts
@@ -1,16 +1,18 @@
-import { UserEntity } from '../../../src/domain/user/user.entity'
-import { UserProps } from '../../../src/shared/interface/user/user-props.interface'
-import { UserDataBuilder } from '../../../src/util/testing/user-data-builder'
+import { UserEntity } from '../../../../src/domain/user/user.entity'
+import { UserProps } from '../../../../src/shared/interface/user/user-props.interface'
+import { UserDataBuilder } from '../../../../src/util/testing/user-data-builder'
 describe('UserEntity unit tests', () => {
   let props: UserProps
   let sut: UserEntity
 
   beforeEach(() => {
+    UserEntity.validate = jest.fn()
     props = UserDataBuilder({})
     sut = new UserEntity(props)
   })
 
   it('Constructor method', () => {
+    expect(UserEntity.validate).toHaveBeenCalled()
     expect(sut.props.name).toEqual(props.name)
     expect(sut.props.email).toEqual(props.email)
     expect(sut.props.password).toEqual(props.password)
@@ -47,11 +49,13 @@ describe('UserEntity unit tests', () => {
   })
 
   it('Should update a user', () => {
+    expect(UserEntity.validate).toHaveBeenCalled()
     sut.update('other name')
     expect(sut.props.name).toEqual('other name')
   })
 
   it('Should update the password field', () => {
+    expect(UserEntity.validate).toHaveBeenCalled()
     sut.updatePassword('other password')
     expect(sut.props.password).toEqual('other password')
   })
